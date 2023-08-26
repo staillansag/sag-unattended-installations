532d39ea2d474e6fc10b3bf537c4bdb73ab8e383
staillansag <103417302+staillansag@users.noreply.github.com>
2023-08-26 13:15:12 +0200

    .

--- 01.scripts/installation/setupFunctions.sh
+++ 01.scripts/installation/setupFunctions.sh
@@ -89,7 +89,7 @@ installProducts() {
     logD "Dumping the install.wmscript file into the session audit folder..."
     if [ "${SUIF_DEBUG_ON}" -ne 0 ]; then
       cp /dev/shm/install.wmscript "${SUIF_AUDIT_SESSION_DIR}/"
-      cat ${SUIF_AUDIT_SESSION_DIR}/debugInstall.log
+      tail ${SUIF_AUDIT_SESSION_DIR}/debugInstall.log
     fi
     return 4
   fi
